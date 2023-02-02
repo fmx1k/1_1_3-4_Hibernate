814b2286879f9875ad75f96b48adea44efce379e
fmx1k <officialfmx@yandex.ru>
2023-02-02 22:47:31 +0300

    jdbc is ready

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Александр", "Евгеньевич", (byte) 25);
+        userService.saveUser("Марина", "Олеговна", (byte) 35);
+        userService.saveUser("Алексей", "Павлович", (byte) 45);
+        userService.saveUser("Сергей", "Иванович", (byte) 55);
+
+        System.out.println(userService.getAllUsers());
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,92 +1,37 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private static String CREATE_TABLE = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), lastname VARCHAR(100), age TINYINT)";
-    private static String INSERT_USER = "INSERT INTO users(name, lastname, age) VALUES (?, ?, ?);";
-    private static String DELETE_USER = "DELETE FROM users WHERE id = ?";
+    UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement(CREATE_TABLE);
-            ps.execute();
-        } catch (SQLException e) {
-        }
+        userDao.createUsersTable();
     }
 
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement("DROP TABLE users");
-            ps.execute();
-        } catch (SQLException e) {
-        }
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement(INSERT_USER);
-
-            ps.setString(1, name);
-            ps.setString(2, lastName);
-            ps.setByte(3, age);
-
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement(DELETE_USER);
-
-            ps.setLong(1, id);
-
-            ps.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM users");
-            ResultSet resultSet = ps.executeQuery();
-
-            while (resultSet.next()) {
-                long id = resultSet.getLong("id");
-                String name = resultSet.getString("name");
-                String lastname = resultSet.getString("lastname");
-                Byte age = resultSet.getByte("age");
-                User user = new User(name, lastname, age);
-                user.setId(id);
-                userList.add(user);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return userList;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM users");
-
-            ps.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        userDao.cleanUsersTable();
     }
 }
